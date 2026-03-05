ac50b18ba7b32bc71c8434dd736c33cad4756ced
Shin Kuboaki <kuboaki@gmail.com>
2026-03-05 09:55:59 +0900

    PDFのsidebar用ライブラリを変更してタイトルがないときは「コラム」をつけないようにした

--- lib/pdf-converter-custom-sidebar-caption.rb
+++ lib/pdf-converter-custom-sidebar-caption.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: false
 
+# Custom Converter for Sidebar
 class CustomSidebarPDFConverter < (Asciidoctor::Converter.for 'pdf')
   register_for 'pdf'
 
-  def convert_sidebar node
+  def convert_sidebar(node)
     # warn 'Converting sidebar node...'
     # warn node.caption, node.title
-    if(node.caption)
+    if node.caption
       node.title = %(【#{node.caption}】 #{node.title})
-    else
+    elsif node.title
       node.title = %(【#{node.title}】)
     end
     super
   end
-
 end
